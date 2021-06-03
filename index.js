a480dce7e7811952976323fee1d0e526a1728f06
Antonio Lopez <antonio.lopez-navarro@cegom.de>
2021-06-03 10:50:21 +0200

    new changes: - schemas are not readed by default - ajv upgraded to latest version - You sould configure the scemas manual

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { authorizeHttpParams } = require("./lib/authorizeHttpParams");
+const { authorizeHttpParams, getAJVLastError, configSchema } = require("./lib/authorizeHttpParams");
 const { defaultErrorHandler } = require("./lib/errorHandler");
 const runWithMiddlewares = require("./lib/runWithMiddlewares");
 
 exports.runWithMiddlewares = runWithMiddlewares;
 exports.authorizeHttpParams = authorizeHttpParams;
 exports.defaultErrorHandler = defaultErrorHandler;
+exports.getAJVLastError = getAJVLastError;
+exports.configSchema = configSchema;
