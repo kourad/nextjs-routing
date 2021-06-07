0d3aecdd36fd80e1bc2495f0bd6d1cd50e11aaae
Antonio Lopez <antonio.lopez-navarro@cegom.de>
2021-06-07 15:25:57 +0200

    add conditional schema

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { authorizeHttpParams, getAJVLastError, configSchema } = require("./lib/authorizeHttpParams");
+const { authorizeHttpParams, getAJVLastError } = require("./lib/authorizeHttpParams");
+const conditionalSchema = require("./lib/conditionalSchema");
 const { defaultErrorHandler } = require("./lib/errorHandler");
 const runWithMiddlewares = require("./lib/runWithMiddlewares");
+const { configSchema } = require("./lib/schemaTools");
 
 exports.runWithMiddlewares = runWithMiddlewares;
 exports.authorizeHttpParams = authorizeHttpParams;
 exports.defaultErrorHandler = defaultErrorHandler;
 exports.getAJVLastError = getAJVLastError;
 exports.configSchema = configSchema;
+exports.conditionalSchema = conditionalSchema;
